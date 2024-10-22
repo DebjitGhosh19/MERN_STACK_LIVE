fc1b39b9d8bad9ecc8e593fe87f66fd7d97a8996
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2024-10-22 21:14:38 +0530

    class-68

---
+++ Node Js and Express Js/4-Npm and tool/app.js
@@ -0,0 +1,15 @@
+const http=require('http');
+const RequestHandelar = require('./RequestHandelar');
+
+const arr=[5,8,7,2,9];
+for (let index = 0; index < arr.length; index++) {
+  const element = arr[index];
+  console.log(element);
+  
+}
+const server=http.createServer(RequestHandelar);
+const port=3000;
+server.listen(port,()=>{
+  console.log(`server starting at: http//localhost:${port}`);
+  
+})
