7c69e2b5f70e4c13e5cc49effe46f74c9a9903f3
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2024-12-06 23:33:25 +0530

    class-78

--- Node Js and Express Js/12.1-momgodb/controllers/hostController.js
+++ Node Js and Express Js/12.1-momgodb/controllers/hostController.js
@@ -48,18 +48,14 @@ if (!editing) {
 
 exports.postEditHome=(req,res,next)=>{
   const {id,houseName,rent,location,rating,photoUrl,description}=req.body;
-  const newHome = new Home(houseName,rent,location,rating,photoUrl,description);
-  newHome.id=id;
-  // newHome.save(err=>{
-  //   if (err) {
-  //   console.log("Error while updating home",err);      
-  //   } else {
-  //     res.redirect("host-homes")
-  //   }
-    
-  //  })
-  newHome.save().then(([rows])=>{
-    res.redirect("host-homes")
+  const newHome = new Home(houseName,rent,location,rating,photoUrl,description,id);
+  
+  newHome.save().then(error=>{
+    if (error) {
+      console.log("Error while updating home",error);
+      
+    }
+    res.redirect("/host/host-homes")
   })
 }
 exports.postDeleteHome=(req,res,next)=>{
--- Node Js and Express Js/12.1-momgodb/models/Home.js
+++ Node Js and Express Js/12.1-momgodb/models/Home.js
@@ -4,19 +4,27 @@ const { getDb } = require('../utils/Database-utils');
 
 
 module.exports=class Home {
-constructor(houseName,rent,location,rating,photoUrl,description) {
+constructor(houseName,rent,location,rating,photoUrl,description,_id) {
 this.houseName=houseName;
 this.rent=rent;
 this.location=location;
 this.rating=rating;
 this.photoUrl=photoUrl;
 this.description=description;
+if (_id) {
+  this._id=new ObjectId(String(_id))
+}
   }
   save(){
     const db = getDb();
-    return db.collection("homes").insertOne(this).then(result => {
-      console.log(result);
-    })
+    if(this._id){
+      return db.collection("homes").updateOne({_id:this._id},
+        {$set:this})
+    }
+    else{
+      return db.collection("homes").insertOne(this)
+        }
+   
   }
  static fetchAll(){
   const db = getDb();
@@ -27,17 +35,23 @@ this.description=description;
   static findById(homeId){
     const db = getDb();
     return db.collection("homes").find({_id: new ObjectId(String( homeId))}).next()
-    .then(home=>{
-      console.log(home);
-      return home;
-    })
-    .catch(error=>{
-      console.log("Error came while doing findById",error);
-      
-    })
+   
   }
 
   static deleteById(homeId){
-    
+   const db=getDb();
+   return db
+   .collection("homes")
+   .deleteOne({_id: new ObjectId(String(homeId))});
+
 }
 }
+
+// .then(home=>{
+//   console.log(home);
+//   return home;
+// })
+// .catch(error=>{
+//   console.log("Error came while doing findById",error);
+  
+// })
