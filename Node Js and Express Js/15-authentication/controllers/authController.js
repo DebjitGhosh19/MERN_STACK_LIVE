89d48c5168eaa1712cc44c3997a6fe51d0663591
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2024-12-18 20:30:54 +0530

    class-82

--- Node Js and Express Js/15-authentication/controllers/authController.js
+++ Node Js and Express Js/15-authentication/controllers/authController.js
@@ -1,3 +1,5 @@
+const {check, validationResult}=require('express-validator');
+
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", { pagetitle: "Login", isLoggedIn: false });
 };
@@ -12,8 +14,38 @@ exports.postLogout = (req, res, next) => {
 exports.getsignup = (req, res, next) => {
   res.render("auth/signup", { pagetitle: "signup", isLoggedIn: false });
 };
-exports.postsignup= (req, res, next) => {
-  console.log('Signup',req.body);
-  
+let firstNameValidator=
+
+exports.postsignup= [
+  //First Name validatiom
+  check('firstName')
+.notEmpty()
+.withMessage('First name is required')
+.trim()
+.isLength({min:2})
+.withMessage("First name must be at last 2 characters long")
+.matches(/^[a-zA-Z\s]+$/)
+.withMessage('First name can only contain letters'),
+
+ //Last Name validatiom
+ check('lastName')
+ .notEmpty()
+ .withMessage('Last name is required')
+ .trim()
+ .isLength({min:2})
+ .withMessage("Last  name must be at last 2 characters long")
+ .matches(/^[a-zA-Z\s]*$/)
+ .withMessage('Last  name can only contain letters'),
+  (req, res, next) => {
+    console.log('Signup',req.body);
+    const errors=validationResult(req);
+if(!errors.isEmpty()){
+  return res.status(422).render("auth/signup", { 
+     pagetitle: "signup",
+     isLoggedIn: false,
+     errorMessages:errors.array().map(err=>err.msg),
+    });
+}
   res.redirect("/login");
-};
+  },
+]
