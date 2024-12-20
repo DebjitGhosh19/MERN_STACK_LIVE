a6e26e180b33320da2e8908fb095e7d8c7ba9325
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2024-12-20 19:11:24 +0530

    practice

--- Final/2_Js/1_Day/one.js
+++ Final/2_Js/1_Day/one.js
@@ -1,1 +1,18 @@
-alert("Welcome");
+
+let value=0;
+const para=document.querySelector('#para');
+
+function AddToBag(){
+  value+=1;
+  para.innerHTML=`Your Bag has ${value} items`
+}
+
+function AddTwo(){
+  value+=2;
+  para.innerHTML=`Your Bag has ${value} items`
+}
+
+function wishlist(){
+  value-=1;
+  para.innerHTML=`Your Bag has ${value} items`
+}
