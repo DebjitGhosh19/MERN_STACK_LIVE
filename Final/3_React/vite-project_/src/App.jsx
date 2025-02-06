8a21ce012e23dae7fc5589a954cc77d5d2e7bd22
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-02-06 22:04:45 +0530

    p

---
+++ Final/3_React/vite-project_/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+
+  const  myHeading={
+    color:"white",
+      background:"green"
+  }
+
+  return (
+    <>
+      <h1 className="heading" style={myHeading}>  
+ App Heading
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
