ba609345a2457953a9e45d634ac23ac5af9ea04c
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-04-08 23:18:43 +0530

    p

--- Final/2_Js/js quesions/Four.js
+++ Final/2_Js/js quesions/Four.js
@@ -1,7 +1,11 @@
 const calMax=(numbers)=>{
-  return numbers.reduce((accumulator, currentValue) =>accumulator>currentValue?accumulator:currentValue,0)
-    
-   }
-   
+  return numbers.reduce((accumulator, currentValue) =>{
+    if (accumulator>currentValue) {
+      return accumulator
+    } else {
+      return currentValue
+    }
+  },0)
+   }  
    console.log(calMax([1,2,3,4,5,88,5,74,99]));
   
---
+++ Final/2_Js/js quesions/six.js
@@ -0,0 +1,20 @@
+const array=[
+  {
+    id:1,
+    name:"Dip",
+    age:28
+  },
+  {
+    id:2,
+    name:"Sayan",
+    age:22
+  },
+  {
+    id:3,
+    name:"Priya",
+    age:24
+  }
+]
+const assAge=array.sort((a,b)=>a.age-b.age)
+console.log(assAge);
+
