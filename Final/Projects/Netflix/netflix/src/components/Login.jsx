ff988dfe40d0574e0e704abe25ab4b272c8f8424
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-04-16 23:53:47 +0530

    P

--- Final/Projects/Netflix/netflix/src/components/Login.jsx
+++ Final/Projects/Netflix/netflix/src/components/Login.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 import backgroundImage from '../assets/netflix-background.jpg'
 const Login = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [fullname, setfullname] = useState("");
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+const handelInputData=(e)=>{
+  e.preventDefault();
+  console.log(fullname,email,password);
+  
+}
   return (
     <div   className="h-screen bg-cover bg-center"
     style={{ backgroundImage: `url(${backgroundImage})` }}>
    <Header/>
-   <form action="" className='bg-black w-3/12 text-white mx-auto mt-[10%] p-4 flex-col opacity-90 '>
-    <h1 className='text-center font-medium text-4xl'>Sign In</h1>
-    <div className='flex flex-col m-auto'>
-    <input type="text" placeholder='Full Name'  className='bg-gray-800 p-4 m-4'/>
-    <input className='bg-gray-800 p-4 m-4' type="email" name="email" id="email" placeholder='Email' />
-    <input className='bg-gray-800 p-4 m-4' type="password" name="password"  id="password" placeholder='Password' />
+   <form onSubmit={handelInputData} action="" className='bg-black w-3/12 text-white mx-auto mt-[5%]  flex-col opacity-90 p-12 min-w-[300px] rounded-md'>
+   <h1 className='text-center font-medium text-4xl mb-2'>{isLogin?"Login":"Signup"}</h1>
+    <div className='flex flex-col'>
+    {!isLogin&&<input value={fullname} onChange={(e)=>setfullname(e.target.value)} type="text" placeholder='Full Name'  className='bg-gray-800 p-4 m-2 focus:outline-none'/>}
+    <input value={email} onChange={(e)=>setemail(e.target.value)} className='bg-gray-800 p-4 m-2 focus:outline-none' type="email" name="email" id="email" placeholder='Email' />
+    <input value={password} onChange={(e)=>setpassword(e.target.value)} className='bg-gray-800 p-4 m-2 focus:outline-none' type="password" name="password"  id="password" placeholder='Password' />
+    <button className='bg-red-600 p-4 m-2'>{isLogin?"Login":"Signup"}</button>
+    <p className='m-4 '>{isLogin?"New to Netflix?":"Already have an account?"} <span className='ml-1 text-blue-700 font-medium cursor-pointer' onClick={()=>setIsLogin(!isLogin)}>{isLogin?"Signup":"Login"}</span> </p>
     </div>
    </form>
     </div>
