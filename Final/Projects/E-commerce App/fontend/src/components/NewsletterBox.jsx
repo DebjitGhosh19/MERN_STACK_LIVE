4f8567dc149137d263d6fe921393f4956915d8d2
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-05-11 22:58:15 +0530

    p

---
+++ Final/Projects/E-commerce App/fontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+  return (
+    <div className='text-center'>
+      <p className='font-medium text-2xl text-gray-800 '>Subscribe now & get 20% off</p>
+      <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      <form action="" className='border flex w-full sm:1/2 items-center gap-3 mx-auto my-6 pl-3'>
+        <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required />
+        <button className='bg-black text-white  cursor-pointer text-xs px-10 py-4'>SUBSCRIBE</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewsletterBox
