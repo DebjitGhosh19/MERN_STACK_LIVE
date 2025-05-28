02bc757d8238427f33482b7dae63d31a511ef6fd
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-05-28 22:02:22 +0530

    p

--- Final/Projects/P_1/fontend/src/components/NewsLetterBox.jsx
+++ Final/Projects/P_1/fontend/src/components/NewsLetterBox.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
 const NewsLetterBox = () => {
+  const onSubmitHandelar=(event)=>{
+    event.preventDefault();
+
+  }
   return (
     <div className='text-center flex flex-col items-center justify-center gap-2 my-10'>
       <p className='font-medium text-2xl '>Subscribe now & get 20% off</p>
       <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <form className='outline w-full sm:w-1/2  items-center flex gap-3  my-6 border'>
-        <input className='p-2 w-full sm:flex-1 outline-none' type="email"  placeholder='Enter your email'/>
-        <button className=' px-10 py-4 bg-black text-white uppercase text-xs ' type="submit">Subscribe</button>
+      <form onSubmit={onSubmitHandelar} className='outline w-full sm:w-1/2  items-center flex gap-3  my-6 border cursor-pointer'>
+        <input className='p-2 w-full sm:flex-1 outline-none' type="email"  placeholder='Enter your email' required/>
+        <button className=' px-10 py-4 bg-black text-white uppercase text-xs cursor-pointer' type="submit">Subscribe</button>
       </form>
     </div>
   )
--- Final/Projects/P_1/fontend/src/pages/Collection.jsx
+++ Final/Projects/P_1/fontend/src/pages/Collection.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-
+import React, { useContext, useState } from "react";
+import { assets } from "../assets/assets";
+import { ShopContext } from "../context/ShowpConext";
 const Collection = () => {
-  return (
-    <div>Collection</div>
-  )
-}
+    const { products} = useContext(ShopContext);
+  const [VisableFilter, setVisableFilter] = useState(false);
 
-export default Collection
+
+  
+  return (
+   
+    
+    <div>
+      <hr />
+      <div className="flex flex-col sm:flex-row py-10 gap-2">
+        {/* left Side */}
+        <div className={`sm:w-96 flex flex-col gap-4 `}>
+          <div className="flex gap-2 items-center">
+            <p className="text-xl ">FILTERS</p>
+            <img className="w-2 cursor-pointer sm:hidden " onClick={(e)=>setVisableFilter(!VisableFilter)} src={assets.dropdown_icon} alt="" />
+          </div>
+          <div className={` ${VisableFilter ? "" : "hidden"} sm:flex flex-col`}>
+            <div className="flex flex-col gap-2 border border-gray-300 p-4 mb-4">
+              <p className="font-semibold">CATAGORIES</p>
+              <div className="flex gap-2 text-gray-400">
+                <input type="checkbox" name="men" id="" />
+                <label htmlFor="men">Men</label>
+              </div>
+              <div className="flex gap-2 text-gray-400">
+                <input type="checkbox" name="women" id="" />
+                <label htmlFor="women">Women</label>
+              </div>
+              <div className="flex gap-2 text-gray-400">
+                <input type="checkbox" name="kids" id="" />
+                <label htmlFor="kids">Kids</label>
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 border border-gray-300 p-4">
+              <p className="font-semibold">TYPE</p>
+              <div className="flex gap-2 text-gray-400">
+                <input type="checkbox" name="men" id="" />
+                <label htmlFor="Topwear">Topwear</label>
+              </div>
+              <div className="flex gap-2 text-gray-400">
+                <input type="checkbox" name="women" id="" />
+                <label htmlFor="Bottomwear">Bottomwear</label>
+              </div>
+              <div className="flex gap-2 text-gray-400">
+                <input type="checkbox" name="kids" id="" />
+                <label htmlFor="Winterwear">Winterwear</label>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* right Side */}
+        <div className=" w-full  p-4">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center gap-2 ">
+              <p>ALL COLLECTION</p>
+              <p className="w-8 h-[2px] bg-black "></p>
+            </div>
+            <select name="" id="">
+              <option value="relavent">Short by:Relevent</option>
+              <option value="relavent">Short by:Low to High</option>
+              <option value="relavent">Short by: High to Low</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Collection;
