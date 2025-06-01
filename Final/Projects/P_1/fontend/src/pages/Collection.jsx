5493589062fd471642bda7a2502a21bd972a13c2
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-06-01 21:28:22 +0530

    P

--- Final/Projects/P_1/fontend/src/pages/Collection.jsx
+++ Final/Projects/P_1/fontend/src/pages/Collection.jsx
@@ -1,9 +1,18 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShowpConext";
+import ProductsItem from "../components/ProductsItem";
 const Collection = () => {
     const { products} = useContext(ShopContext);
   const [VisableFilter, setVisableFilter] = useState(false);
+  
+const [FilterData, setFilterData] = useState([]);
+useEffect(() => {
+  products.filter((item) => {
+
+  })
+    
+}, [])
 
 
   
@@ -54,17 +63,24 @@ const Collection = () => {
         </div>
         {/* right Side */}
         <div className=" w-full  p-4">
-          <div className="flex justify-between items-center">
-            <div className="flex items-center gap-2 ">
+          <div className="flex justify-between items-center gap-2">
+            <div className="flex items-center gap-1 sm:gap-2  ">
               <p>ALL COLLECTION</p>
               <p className="w-8 h-[2px] bg-black "></p>
             </div>
-            <select name="" id="">
+            <select name="" id="" className="border border-gray-300 p-2 rounded-md">
               <option value="relavent">Short by:Relevent</option>
               <option value="relavent">Short by:Low to High</option>
               <option value="relavent">Short by: High to Low</option>
             </select>
           </div>
+        <div className="grid grid-cols-2  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+            {
+            products.map((item)=>(
+              <ProductsItem id={item._id} image={item.image} name={item.name} price={item.price}/>
+            ))
+          }
+        </div>
         </div>
       </div>
     </div>
