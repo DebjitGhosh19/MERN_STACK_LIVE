794fa15422891b8a03671fc00be47469f2789a3b
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-06-15 21:57:41 +0530

    P

--- Final/Projects/P_1/fontend/src/pages/Cart.jsx
+++ Final/Projects/P_1/fontend/src/pages/Cart.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/ShowpContext'
 
 const Cart = () => {
+  const {products,CartItems}=useContext(ShopContext);
+  const [CartData, setCartData] = useState([]);
+  useEffect(() => {
+    const tempData=[];
+    for (const items in CartItems) {
+      for(const item in CartItems[items]){
+        if (CartItems[items][item]>0) {
+          tempData.push({
+            _id:items,
+            size:item,
+            quantity:CartItems[items][item]
+          })
+        }
+      }      
+      console.log(tempData);
+      
+      setCartData(tempData)    
+    }
+  },[CartItems])
+  
   return (
     <div>Cart</div>
   )
