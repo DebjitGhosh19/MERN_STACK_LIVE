acb3c3bf4c757b9c90a4f7d968b79b439d270d65
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-08-02 06:32:46 +0530

    p

---
+++ Final/Projects/P_1/backend/controllers/orderController.js
@@ -0,0 +1,26 @@
+//Place orders using COD Method
+export const PlaceOrder=async (req,res) => {
+  
+}
+//Place orders using Stripe Method
+export const PlaceOrderStripe=async (req,res) => {
+  
+}
+
+//Place orders using Razorpay Method
+export const PlaceOrderRazorpay=async (req,res) => {
+  
+}
+//All orders data for Admin panel
+export const allOrders=async (req,res) => {
+  
+}
+//User order data for fontend
+export const userOrders=async (req,res) => {
+  
+}
+//update order status from admin panel
+
+export const updateStatus=async (req,res) => {
+  
+}
