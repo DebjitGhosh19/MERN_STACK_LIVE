a2b6923e1f0d1712ea5e481d8e2676ff35e96698
Debjit Ghosh <ghoshdebjit.2002@gmail.com>
2025-08-03 22:31:41 +0530

    p

--- Final/Projects/P_1/fontend/src/components/Hero.jsx
+++ Final/Projects/P_1/fontend/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className='flex flex-col sm:flex-row border  border-gray-400'>
         {/* Left Side Hero Section  */}
         <div className='flex flex-col items-center w-full sm:w-1/2 justify-center py-2 sm:py-10'>
-          <div className='flex  items-center  gap-2 '>
+         <div> <div className='flex  items-center  gap-2 '>
             <p className='w-8 sm:w-11 h-[2px] bg-gray-400'></p>
             <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
           </div>
@@ -13,7 +13,7 @@ const Hero = () => {
           <div className='flex items-center gap-2 '>
             <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
             <p className='w-8 sm:w-11 h-[2px] bg-gray-400'></p>
-          </div>
+          </div></div>
         </div>
         <img className='flex flex-col items-center w-full sm:w-1/2 justify-center' src={assets.hero_img} alt="" />
     </div>
